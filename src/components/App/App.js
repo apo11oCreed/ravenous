7dbeb07644d2dfb2692e9e7e7f2ded8fe7aedfe1
nc <nelson.ncdesigns@gmail.com>
2021-12-29 01:27:15 -0500

    Ravenous: Create Project & Components II

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,12 +3,26 @@ import BusinessList from '../BusinessList/BusinessList';
 import SearchBar from '../SearchBar/SearchBar';
 import './App.css';
 
+const business={
+  imageSrc: 'https://content.codecademy.com/programs/react/ravenous/pizza.jpg',
+  name: 'MarginOtto Pizzeria',
+  address: '1010 Paddington Way',
+  city: 'Flavortown',
+  state: 'NY',
+  zipCode: '10101',
+  category: 'Italian',
+  rating: 4.5,
+  reviewCount: 90
+}
+
+const businesses=[business,business,business,business,business];
+
 function App() {
   return (
     <div className="App">
   <h1>ravenous</h1>
   <SearchBar />
-  <BusinessList />
+  <BusinessList businesses={businesses} />
 </div>
   );
 }
